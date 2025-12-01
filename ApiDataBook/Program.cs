52e849024a28e5abae2c752d499b125ccba1d7a0
mateusafgamedev <mateusaf.gamedev@gmail.com>
2025-12-01 18:50:31 -0300

    Criando AppDbContext para comunicação com banco de dados e criando entidades autor e livros

--- ApiDataBook/Program.cs
+++ ApiDataBook/Program.cs
@@ -1,3 +1,6 @@
+using ApiDataBook.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>( options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConection"));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +23,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
 
 app.UseHttpsRedirection();
 
