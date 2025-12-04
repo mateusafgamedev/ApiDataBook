7c2f39bd4044334287dc42118d35477df1230d2d
mateusafgamedev <mateusaf.gamedev@gmail.com>
2025-12-04 17:37:09 -0300

    Finalização da API

--- ApiDataBook/Controllers/LivroController.cs
+++ ApiDataBook/Controllers/LivroController.cs
@@ -1,4 +1,5 @@
-﻿using ApiDataBook.Model;
+﻿using ApiDataBook.Dto.Livro;
+using ApiDataBook.Model;
 using ApiDataBook.Services.Livro;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -11,8 +12,8 @@ namespace ApiDataBook.Controllers
     {
 
         private readonly ILivroInterface _livroInterface;
-        public LivroController(ILivroInterface livroInterface) 
-        { 
+        public LivroController(ILivroInterface livroInterface)
+        {
             _livroInterface = livroInterface;
         }
 
@@ -20,13 +21,41 @@ namespace ApiDataBook.Controllers
         public async Task<ActionResult<ResponseModel<List<LivroModel>>>> ListarLivros()
         {
             var livros = await _livroInterface.ListarLivros();
-            return Ok(livros);  
+            return Ok(livros);
         }
 
         [HttpGet("BuscarLivroPorId/{idLivro}")]
         public async Task<ActionResult<ResponseModel<LivroModel>>> BuscarLivroPorId(int idLivro)
         {
             var livro = await _livroInterface.BuscarLivroPorId(idLivro);
+            return Ok(livro);
+        }
+
+        [HttpGet("BuscarLivroPorIdAutor/{idAutor}")]
+        public async Task<ActionResult<ResponseModel<List<LivroModel>>>> BuscarLivroPorIdAutor(int idAutor)
+        {
+            var livro = await _livroInterface.BuscarLivroPorIdAutor(idAutor);
+            return Ok(livro);
+        }
+
+        [HttpPost("CadastrarLivro")]
+        public async Task<ActionResult<ResponseModel<List<LivroModel>>>> CadastrarLivro(CadastrarLivroDto livroDto)
+        {
+            var livro = await _livroInterface.CadastrarLivro(livroDto);
+            return Ok(livro);
+        }
+
+        [HttpPut("EditarLivro")]
+        public async Task<ActionResult<ResponseModel<List<LivroModel>>>> EditarLivro(EditarLivroDto livroDto)
+        {
+            var livro = await _livroInterface.EditarLivro(livroDto);
+            return Ok(livro);
+        }
+
+        [HttpDelete("DeletarLivro/{idLivro}")]
+        public async Task<ActionResult<ResponseModel<LivroModel>>> DeletarLivro(int idLivro)
+        {
+            var livro = await _livroInterface.DeletarLivro(idLivro);
             return Ok(livro);
         }
     }
--- ApiDataBook/Services/Livro/ILivroInterface.cs
+++ ApiDataBook/Services/Livro/ILivroInterface.cs
@@ -1,4 +1,5 @@
-﻿using ApiDataBook.Model;
+﻿using ApiDataBook.Dto.Livro;
+using ApiDataBook.Model;
 
 namespace ApiDataBook.Services.Livro
 {
@@ -6,5 +7,9 @@ namespace ApiDataBook.Services.Livro
     {
         Task<ResponseModel<List<LivroModel>>> ListarLivros();
         Task<ResponseModel<LivroModel>> BuscarLivroPorId(int idLivro);
+        Task<ResponseModel<List<LivroModel>>> CadastrarLivro(CadastrarLivroDto livroDto);
+        Task<ResponseModel<List<LivroModel>>> BuscarLivroPorIdAutor(int iAutor);
+        Task<ResponseModel<List<LivroModel>>> EditarLivro(EditarLivroDto livroDto);
+        Task<ResponseModel<LivroModel>> DeletarLivro(int idLivro);
     }
 }
